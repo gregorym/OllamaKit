ee93c60485f5343aca7578951e3170e7eaa33c2e
Gregory Marcilhacy <g.marcilhacy@gmail.com>
2025-05-20 15:38:13 -0700

    Update OKGenerateEmbeddingsRequestData.swift

--- Sources/OllamaKit/RequestData/OKGenerateEmbeddingsRequestData.swift
+++ Sources/OllamaKit/RequestData/OKGenerateEmbeddingsRequestData.swift
@@ -12,8 +12,8 @@ public struct OKGenerateEmbeddingsRequestData: Encodable {
     /// A string representing the identifier of the model.
     public let model: String
     
-    /// A string containing the initial input or prompt.
-    public let input: String
+    /// A list of strings containing the initial input or prompt.
+    public let input: [String]
     
     /// Optional ``OKCompletionOptions`` providing additional configuration for the generation request.
     public var options: OKCompletionOptions?
@@ -21,7 +21,7 @@ public struct OKGenerateEmbeddingsRequestData: Encodable {
     /// Optionally control how long the model will stay loaded into memory following the request (default: 5m)
     public var keep_alive: String?
     
-    public init(model: String, input: String) {
+    public init(model: String, input: [String]) {
         self.model = model
         self.input = input
     }
